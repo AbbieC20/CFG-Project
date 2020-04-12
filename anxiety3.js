e70b9fb9f7e4d9c247a225240650e9ad918b6fcb
Liz <lizgreen987@gmail.com>
2020-04-12 14:26:42 +0100

    Update anxiety3.js

--- anxiety3.js
+++ anxiety3.js
@@ -1,11 +1,11 @@
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 30px from the top of the document, show the button
+// When the user scrolls down 50px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     mybutton.style.display = "block";
   } 
   else {
